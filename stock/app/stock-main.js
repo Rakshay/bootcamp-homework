c82942a5dd95d42f15002b3aacf785f6e2039b2a
krishna Agarwal <agarwal.krishna240@gmail.com>
2016-02-08 12:07:41 +0530

    used keyup function

--- stock/app/stock-main.js
+++ stock/app/stock-main.js
@@ -3,6 +3,31 @@ $(document).ready(function()
 {
 	 var source=$("#entry-template").html();
 		var template=Handlebars.compile(source);
+		var update=function()
+		{
+			var from = $('#fromDropDown').val();
+		var to = $('#toDropDown').val();
+		var value=$('#valueInput').val();
+		 $.ajax({
+		    async: true,
+		    crossDomain: true,
+		    url:'https://currencyconverter.p.mashape.com/?from='+from+'&from_amount='+value+'&to='+to,
+		    type: 'GET',
+		    data: {},
+		    dataType: 'json',
+		    success: function (data) {
+		      console.log(data.to_amount);
+		      $('#valueOutput').val(data.to_amount);
+		    },
+		    error: function (err) {
+		      console.log(err);
+		      //alert(err);
+		    },
+		    beforeSend: function (xhr) {
+		      xhr.setRequestHeader('X-Mashape-Key', 'Bs5BvTwMeNmshIVgyxatfWRfPMkNp1Dmi30jsnLUNZ8zyDyBW8');
+		    }
+		});
+		};
 	 $.ajax(
 	{
 	    async: true,
@@ -31,29 +56,6 @@ $(document).ready(function()
 	
 		
 
-	$('#valueInput,#fromDropDown,#toDropDown').on('change',function()
-	{
-		var from = $('#fromDropDown').val();
-		var to = $('#toDropDown').val();
-		var value=$('#valueInput').val();
-		 $.ajax({
-		    async: true,
-		    crossDomain: true,
-		    url:'https://currencyconverter.p.mashape.com/?from='+from+'&from_amount='+value+'&to='+to,
-		    type: 'GET',
-		    data: {},
-		    dataType: 'json',
-		    success: function (data) {
-		      console.log(data.to_amount);
-		      $('#valueOutput').val(data.to_amount);
-		    },
-		    error: function (err) {
-		      console.log(err);
-		      //alert(err);
-		    },
-		    beforeSend: function (xhr) {
-		      xhr.setRequestHeader('X-Mashape-Key', 'Bs5BvTwMeNmshIVgyxatfWRfPMkNp1Dmi30jsnLUNZ8zyDyBW8');
-		    }
-  		});
-	});  
+	$('#fromDropDown,#toDropDown').on('change',update);
+	$('#valueInput').on('keyup',update);
 });
